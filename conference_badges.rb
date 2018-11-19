e8db62d4c3a326b266c35087579656c9bfcda8b6
Marie Burns <mcburns2222@gmail.com>
2018-11-19 20:30:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,14 +1,15 @@
 def batch_badge_creator
   speakers = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
-  speaker.each do |name|
+  speaker.collect do |name|
     def badge_maker(name)
       puts "Hello my name is #{name}."
     end
-    room = 7
-    def assign_rooms(name)
+    
+    def assign_rooms
+      room = 7
+      speakers.collect do |name|
       puts "Hello, #{name}! You'll be assigned to room #{room}!"
       room -= 1 
     end
-      
   end
 end
