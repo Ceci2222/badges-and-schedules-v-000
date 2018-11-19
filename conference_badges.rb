79aef03f178bab164cc6c4556269dd58a2a8946b
Marie Burns <mcburns2222@gmail.com>
2018-11-19 21:24:19 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,6 +17,7 @@ def assign_rooms(array)
 end
 
 def printer(array)
+  speakers = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
   batch_badge_creator.each do |badge|
     puts "#{badge}"
   end
